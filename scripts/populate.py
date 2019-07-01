beeba10baa2b3199dffe68ed176b11d7aa092a67
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-07-01 07:22:12 -0700

    Adds container

---
+++ scripts/populate.py
@@ -0,0 +1,15 @@
+# /usr/bin/env python
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import time
+
+time.sleep(5)
+es = Elasticsearch()
+
+doc = {
+    'author': 'kimchy',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
+res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
+print(res['result'])
